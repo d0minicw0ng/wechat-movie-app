0b51301e14344b4eb980aebb04a5f27d2990b8b4
Dominic Wong <dominic.wong.617@gmail.com>
2019-04-27 13:49:35 +0800

    Postgres config uses environment variables

--- server/config.js
+++ server/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   pgConfig: {
-    host: 'localhost',
-    database: 'wechat_movies',
+    host: process.env.DATABASE_HOST || 'localhost',
+    database: process.env.DATABASE_NAME || 'wechat_movies',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     port: 5432
   }
 };
