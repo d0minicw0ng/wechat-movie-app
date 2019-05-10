05bca1fce4ff72703c5c9c513d312078f874760d
Dominic Wong <dominic.wong.617@gmail.com>
2019-05-10 22:47:32 +0800

    Fixes my favorite reviews

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -18,7 +18,7 @@ module.exports = {
     const pg = new Client(config.pgConfig);
     await pg.connect();
     // NOTE: I am just going to hard code id to 1 as we don't have a login system.
-    const res = await pg.query('SELECT r.content, r.audio, m.title, m.image, u.username, u.profile_image FROM user_favorite_reviews ufr JOIN reviews r ON ufr.review_id = r.id JOIN movies m ON r.movie_id = m.id JOIN users u ON r.user_id = u.id WHERE ufr.user_id = $1 ORDER BY ufr.created_at DESC', [1]);
+    const res = await pg.query('SELECT r.content, r.audio_url, r.duration, m.title, m.image, u.username, u.profile_image FROM user_favorite_reviews ufr JOIN reviews r ON ufr.review_id = r.id JOIN movies m ON r.movie_id = m.id JOIN users u ON r.user_id = u.id WHERE ufr.user_id = $1 ORDER BY ufr.created_at DESC', [1]);
     const reviews = res.rows;
 
     await pg.end();
